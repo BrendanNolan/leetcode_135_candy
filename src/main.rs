94c03627ce9ad10c4fd254b3fa384ee5ce613ead
Brendan Nolan <bnolan23@gmail.com>
2024-12-22 15:49:13 +0000

    Add more tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// There is a wring assumption throughout - that a section must have length > 1 .
 mod implementation {
     use std::cmp;
 
@@ -179,32 +178,17 @@ mod implementation {
 
         #[test]
         fn test_11() {
-            test_forwards_and_backwards(
-                vec![
-                    58, 21, 72, 77, 48, 9, 38, 71, 68, 77, 82, 47, 25, 94, 89, 54, 26, 54, 54, 99,
-                    64, 71, 76, 63, 81, 82, 60, 64, 29, 51, 87, 87, 72, 12, 16, 20, 21, 54, 43, 41,
-                    83, 77, 41, 61, 72, 82, 15, 50, 36, 69, 49, 53, 92, 77, 16, 73, 12, 28, 37, 41,
-                    79, 25, 80, 3, 37, 48, 23, 10, 55, 19, 51, 38, 96, 92, 99, 68, 75, 14, 18, 63,
-                    35, 19, 68, 28, 49, 36, 53, 61, 64, 91, 2, 43, 68, 34, 46, 57, 82, 22, 67, 89,
-                ],
-                208,
-            );
+            test_forwards_and_backwards(vec![4, 3, 2, 1, 2, 2, 3], 15,);
         }
 
         #[test]
         fn test_12() {
-            assert_eq!(
-                count_sweets(&[1, 2, 2, 3]),
-                6,
-            );
+            test_forwards_and_backwards(vec![1, 2, 2, 3], 6,);
         }
 
         #[test]
         fn test_13() {
-            assert_eq!(
-                count_sweets(&[1, 2, 3, 3, 2, 1]),
-                12,
-            );
+            test_forwards_and_backwards(vec![1, 2, 3, 3, 2, 1], 12,);
         }
     }
 }
